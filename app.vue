bb11b734190cb68456833db2e85fbc6ec48d21af
strun <strun@qq.com>
2024-02-02 21:38:56 +0800

    v1.0

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  components: {},
+  props: {},
+  data (){
+    return {}
+  },
+  computed: {},
+  wathch: {},
+  created(){},
+  mounted(){},
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
